0aec6bc37394dbfa81379b8c7075d23633698c27
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2025-07-12 12:27:00 +0300

    Compare Example

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,7 @@
-let ogrenciIsmi = "Çetin Yılmaz";
-let ogrenciAktifMi = true;
-let ogrenciYas = 20;
+// Karşılatırma, Mantıksal
 
+let a = '5';
+let b = 5;
 
-console.log("Ögreinci İsmi : " + ogrenciIsmi + ", Öğrenci Yaşı : "  + ogrenciYas + ", Öğrenci Aktif Mi ? " + ogrenciAktifMi)
-
-
-// String To Int
-
-let sayi1 = "10";
-let sayi2 = "20";
-
-console.log(typeof(sayi1) + " " + typeof(sayi2) );
-
-let toplam = Number(sayi1) + Number(sayi2);
-
-console.log(typeof(toplam));
-
-console.log(toplam);
-
-// Int To String 
-
-let numbers = 10;
-
-let StringNumber = numbers.toString();
-
-console.log(StringNumber + 10);
-console.log(typeof(StringNumber));
-
-// Undefind
-
-let ogrnNumber;
-console.log(typeof(ogrnNumber));
-
-
-
-
-
-
-
-
+console.log(a == b);  // true
+console.log(a === b); // false
