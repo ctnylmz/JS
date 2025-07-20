d4bb5a070f75ac66e59d06a8c2f0700c6788c3fe
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2025-07-21 01:21:59 +0300

    Example For 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,56 @@
-let meyve = ["Elma", "Armut", "Muz", "Portakal", "Karpuz"]
+const users = [
+    {
+        id: 1,
+        ad: "Çetin",
+        soyad: "Yılmaz",
+        email: "cetin@gmail.com",
+        yas: 25
+    },
+    {
+        id: 2,
+        ad: "Ayşe",
+        soyad: "Kara",
+        email: "ayse.kara@example.com",
+        yas: 30
+    },
+    {
+        id: 3,
+        ad: "Mehmet",
+        soyad: "Demir",
+        email: "mehmet.demir@example.com",
+        yas: 28
+    },
+    {
+        id: 4,
+        ad: "Elif",
+        soyad: "Yıldız",
+        email: "elif.yildiz@example.com",
+        yas: 22
+    },
+    {
+        id: 5,
+        ad: "Ahmet",
+        soyad: "Çelik",
+        email: "ahmet.celik@example.com",
+        yas: 35
+    }
+];
 
-for (let i = 0; i < meyve.length; i++) {
-    console.log(meyve[i]);
+// İndeks kontrolü gerekiyorsa (örneğin: belirli aralıklarla atlamak gibi).
+for (let index = 0; index < users.length; index++) {
+    console.log(users[index].ad);
 }
 
-console.log("------------");
+for (let index in users) {
+    console.log(users[index].ad);
+}
 
-meyve.forEach(meyve => {
-    console.log(meyve);
+
+for (let user of users) {
+    console.log(user.ad);
+}
+
+// Her eleman için bir işlem yapılacaksa.
+users.forEach(user => {
+    console.log(user);
 });
-
