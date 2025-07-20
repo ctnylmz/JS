5c5fff411394516a70d10900826f054714bf9771
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2025-07-20 17:19:21 +0300

    Example Object

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
-const urunler = ["samsung","apple","huawei",5]
+let custumer = {
+    "ad": "Çetin",
+    "soyad": "Yılmaz",
+    "Şehir": "istanbul",
+    "yas": "25",
+    "products": ["Laptop", "phone"]
+}
 
-console.log(urunler);
+console.log(custumer);
+
+console.log(custumer.ad + " " + custumer.soyad + " " + custumer.products[0]);
+
+
+let products = [
+    {
+        "Name": "Apple",
+        "Price": 20
+    },
+    {
+        "Name": "Samsung",
+        "Price": 10
+    },
+     {
+        "Name": "Vestel",
+        "Price": 5
+    }
+]
+
+console.log(products);
+
+
