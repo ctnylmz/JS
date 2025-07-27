d2e397c80c308a3c500dda66cfa39cbe74ad1395
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2025-07-27 16:32:35 +0300

    Example OOP 5
    interface

---
+++ OOP/assets/js/main.ts
@@ -0,0 +1,18 @@
+// interface IIslem {
+//     void Yazdir();
+//     void Sil();
+//     void Duzenle();
+// }
+
+
+// class Fatura : IIslem {
+//     public void Yazdir() {
+//         Console.WriteLine("Fatura yazdırıldı.");
+//     }
+//     public void Sil() {
+//         Console.WriteLine("Fatura silindi.");
+//     }
+//     public void Duzenle() {
+//         Console.WriteLine("Fatura düzenlendi.");
+//     }
+// }
