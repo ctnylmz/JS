589b1387d540f1f092207cab5a79815950507795
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2025-07-27 16:40:13 +0300

    Example OOP 6
    abstract

---
+++ OOP/assets/js/main.js
@@ -0,0 +1,26 @@
+// public abstract class Sekil
+// {
+//     public string Renk { get; set; }
+
+//     // Soyut metot: Gövdesi yok, alt sınıf implement edecek
+//     public abstract double AlanHesapla();
+
+//     // Tamamlanmış metot
+//     public void RenkYazdir()
+//     {
+//         Console.WriteLine($"Renk: {Renk}");
+//     }
+// }
+
+// public class Dikdortgen : Sekil
+// {
+//     public double Genislik { get; set; }
+//     public double Yukseklik { get; set; }
+
+//     // Alt sınıf abstract metodu yazmak zorunda!
+//     public override double AlanHesapla()
+//     {
+//         return Genislik * Yukseklik;
+//     }
+// }
+
