5fd9a00300aead7992f50839b69fd1b5fa47b498
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-01-28 12:29:32 +0300

    Hamburger menu with JavaScript DOM

---
+++ hamburger-menu/app.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector(".menuBtn")
+
+const navbar = document.querySelector(".navbar")
+
+menuBtn.addEventListener("click",function (){
+navbar.classList.toggle("open-navbar")
+})
