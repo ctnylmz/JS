d6b6ba77829c192736acb60c8c0799027fa75df4
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-01-30 12:04:18 +0300

    add function vs arrow function example

--- js-dom-lessons/app.js
+++ js-dom-lessons/app.js
@@ -1,24 +1,20 @@
-// ❌ This function only logs the result to the console
-function calculateTotal2(price, tax) {
-    let total = price + tax;
-    console.log(total); // Logged INSIDE the function (inside scope)
+const colors = ["Red", "Blue", "Blue", "Black", "White"];
+
+function readColors() {
+    for (let i = 0; i < colors.length; i++) {
+        console.log(colors[i]);
+    }
 }
 
-// The returned value is undefined because there is no return
-let orderTotal2 = calculateTotal2(100, 20);
+readColors();
 
-console.log(orderTotal2); 
-// undefined → This log is OUTSIDE the function
+console.log("-------------------");
+/* Arrow Function */
 
+const arrayFunctionReadColors = () => {
+    for (let i = 0; i < colors.length; i++) {
+        console.log(colors[i]);
+    }
+};
 
-// ✅ This function returns the calculated value
-function calculateTotal(price, tax) {
-    let total = price + tax;
-    return total; // Returned to the outside world
-}
-
-// The returned value can be stored and reused
-let orderTotal = calculateTotal(100, 20);
-
-console.log(orderTotal); 
-// 120 → Çünkü değer return ile fonksiyon dışına verildi
+arrayFunctionReadColors();
