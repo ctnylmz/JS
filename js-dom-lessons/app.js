66f8d06647d36584a856a240a1f137aa08b35a27
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-02-01 11:28:44 +0300

    JavaScript DOM text and HTML manipulation examples

--- js-dom-lessons/app.js
+++ js-dom-lessons/app.js
@@ -1,9 +1,16 @@
-const list = document.querySelector(".list");
+const title = document.querySelector("h1");
 
-let students = ["John", "Emma", "Michael", "Sophia", "Daniel"];
+title.innerText = "Hello Javascript Dom";
 
 
-students.forEach(student => {
-    list.innerHTML += `<li>${student}</li>`;
+const list = document.querySelectorAll(".list li");
+
+list.forEach(a => {
+    a.innerText = 'New ' + a.innerText 
 });
+
+
+const finish = document.getElementById("finish");
+finish.innerHTML = '<h4>Finish</h4>'
+
 
