ac08adb5d196c4fc9b6a357755c57a576c95a15f
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-02-04 12:47:46 +0300

    JavaScript DOM Card Toggle Examples

--- js-dom-lessons/app.js
+++ js-dom-lessons/app.js
@@ -1,16 +1,16 @@
-const title = document.querySelector("h1");
+const btn = document.querySelector(".btn");
+const cards = document.querySelectorAll(".card");
 
-title.innerText = "Hello Javascript Dom";
+btn.addEventListener("click",function(){
 
+   const firstHidden = document.querySelector(".card.hidden");
+    
+   firstHidden.classList.remove("hidden")
 
-const list = document.querySelectorAll(".list li");
-
-list.forEach(a => {
-    a.innerText = 'New ' + a.innerText 
 });
 
-
-const finish = document.getElementById("finish");
-finish.innerHTML = '<h4>Finish</h4>'
-
-
+cards.forEach(card => {
+    card.addEventListener("click" , function(){
+        card.classList.add("hidden")
+    })
+})
