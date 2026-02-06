bf71a675a0a15ad81112127096b912dd7fcacf19
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-02-06 11:55:39 +0300

    create vanilla js dom popup example

--- js-dom-lessons/app.js
+++ js-dom-lessons/app.js
@@ -1,9 +1,24 @@
-const card = document.querySelector(".card");
-const inte = document.querySelector(".inte");
+const openBtn = document.querySelector(".open-btn");
+const popup = document.querySelector(".popup-overlay");
+const closeBtn = document.querySelector(".close-btn");
 
 
-card.addEventListener("mousemove", function(e){
 
-    inte.textContent = "X: " + e.offsetX + " Y:" + e.offsetY
+openBtn.addEventListener("click", function(){
 
+    popup.classList.remove("hidden")
+})
+
+
+closeBtn.addEventListener("click", function(){
+
+    popup.classList.add("hidden")
+})
+
+
+popup.addEventListener("click", function (e) {
+    
+  if (e.target === popup) {
+    popup.classList.add("hidden");
+  }
 });
