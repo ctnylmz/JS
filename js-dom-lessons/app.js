e4636a34b2480454afd38ed56c9766bada651d54
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-02-11 11:33:54 +0300

    JavaScript Form Validation With Regex (5–10 Character Limit)

--- js-dom-lessons/app.js
+++ js-dom-lessons/app.js
@@ -1,14 +1,22 @@
+
 const userInput = document.querySelector('input[name="user"]');
 const userForm = document.querySelector('.card');
 const list = document.querySelector('.list');
 
 
+const regexPatern = /^.{5,10}$/;
+
 userForm.addEventListener("submit", function(e){
   e.preventDefault();
-  console.log(userForm.user.value);
-  list.innerHTML = list.innerHTML + `<li>${userForm.user.value}</li>`
-  userForm.user.value = ""
 
+  let succes = regexPatern.test(userForm.user.value);
 
+  if(succes === true){
+    list.innerHTML = list.innerHTML + `<li>${userForm.user.value}</li>`
+   userForm.user.value = ""
+  }else{
+   alert("Must be at least 5 and at most 10 characters.");
+  }
+  
   
 })
