9c7a91bc184048b1657dcebf771e828ef00ef220
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-02-12 11:18:15 +0300

    Feat: Add Inline Success/Error Messages

--- javascript-lessons/js-dom-form/app.js
+++ javascript-lessons/js-dom-form/app.js
@@ -2,21 +2,35 @@
 const userInput = document.querySelector('input[name="user"]');
 const userForm = document.querySelector('.card');
 const list = document.querySelector('.list');
+const message = document.querySelector('.message');
 
 
 const regexPatern = /^.{5,10}$/;
 
-userForm.addEventListener("submit", function(e){
+userForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
   let succes = regexPatern.test(userForm.user.value);
 
-  if(succes === true){
+  if (succes === true) {
+
     list.innerHTML = list.innerHTML + `<li>${userForm.user.value}</li>`
-   userForm.user.value = ""
-  }else{
-   alert("Must be at least 5 and at most 10 characters.");
+    userForm.user.value = ""
+
+    message.textContent = "User added successfully."
+
+    setInterval(() => {
+      message.textContent = "";
+    }, 7000);
+
+  } else {
+
+    message.textContent = "Username must be between 5 and 10 characters."
+
+    setInterval(() => {
+      message.textContent = "";
+    }, 7000);
   }
-  
-  
+
+
 })
