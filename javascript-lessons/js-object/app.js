a855679d622370ed2356e65cda778cb10126450e
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-03-03 16:23:30 +0300

    javascript array of objects example

--- javascript-lessons/js-object/app.js
+++ javascript-lessons/js-object/app.js
@@ -3,6 +3,11 @@ let student = {
     age: 29,
     email: 'canboz@hotmail.com',
     lesson: ['maths', 'chemical', 'physical'],
+    lesson: [
+        {name:"maths",score:90},
+        {name:"chemical",score:100},
+        {name:"physical",score:80}
+    ],
     login: function () {
         console.log("Student signed in successfully.");
     },
