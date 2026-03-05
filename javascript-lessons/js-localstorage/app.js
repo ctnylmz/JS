ed80f76500e076b11c2d573981faf872199e887b
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-03-05 14:59:14 +0300

    javascript localStorage setItem and getItem example

---
+++ javascript-lessons/js-localstorage/app.js
@@ -0,0 +1,12 @@
+localStorage.setItem("name","Çetin")
+localStorage.setItem("age",25)
+
+let name = localStorage.getItem("name")
+let age = localStorage.getItem("age")
+
+console.log(name + " " + age);
+
+
+localStorage.setItem("name","Jack");
+localStorage.age = "30";
+
