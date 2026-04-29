5a9134d35950208087bb35906a50bdca4b1c9ad2
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-04-29 10:50:16 +0300

    updated currency-converter

--- javascript-projects/15-currency-converter/app.js
+++ javascript-projects/15-currency-converter/app.js
@@ -1,60 +1,70 @@
 let one = document.querySelector(".one")
 let two = document.querySelector(".two")
-
 let oneNumberText = document.querySelector(".oneNumberText")
+
 let twoNumberText = document.querySelector(".twoNumberText")
+let final = document.querySelector(".final")
 
 
-
-
-one.value = "TL"
-let oneNumber = 1;
-
-two.value = "USD"
-let twoNumber = 0;
-
-fetch("money.json")
-    .then(response => response.json())
-    .then(data => {
-        twoNumber = data.find(item => item.name === "USD").value;
-    });
-
-oneNumberText.innerHTML = oneNumber
-
-
-fetch("money.json")
-    .then(response => response.json())
-    .then(data => {
-        twoNumberText.innerHTML = data.find(item => item.name === "USD").value;
-    });
-
-
+let state = 0;
 
 one.addEventListener("click", function (e) {
-    console.log(e.target.value)
+
+      fetch("money.json")
+        .then(response => response.json())
+        .then(data => {
+            let ones = data.find(item => item.name === one.value);
+            let twos = data.find(item => item.name === two.value);
+
+            if (ones.value === twos.value) {
+                twoNumberText.innerHTML = 1;
+                state = twoNumberText.innerHTML;
+                final.innerHTML = 1 + " " + one.value + " = " + 1 + " " + two.value
+            } else if (ones.value > twos.value) {
+                twoNumberText.innerHTML = ones.value / twos.value
+                state = twoNumberText.innerHTML
+                final.innerHTML = 1 + " " + one.value + " = " + ones.value / twos.value + " " + two.value
+            } else if (ones.value < twos.value) {
+                twoNumberText.innerHTML = twos.value / ones.value
+                state = twoNumberText.innerHTML
+                final.innerHTML = 1 + " " + one.value + " = " + twos.value / ones.value + " " + two.value
+            }
+
+            console.log(ones.value + " - " + twos.value);
+        });     
 })
+
 
 two.addEventListener("click", function (e) {
 
-fetch("money.json")
-    .then(response => response.json())
-    .then(data => {
-        twoNumberText.innerHTML = data.find(item => item.name === e.target.value).value;
-    });
+          fetch("money.json")
+        .then(response => response.json())
+        .then(data => {
+            let ones = data.find(item => item.name === one.value);
+            let twos = data.find(item => item.name === two.value);
+
+            if (ones.value === twos.value) {
+                twoNumberText.innerHTML = 1;
+                state = twoNumberText.innerHTML;
+                final.innerHTML = 1 + " " + one.value + " = " + 1 + " " + two.value
+            } else if (ones.value > twos.value) {
+                twoNumberText.innerHTML = ones.value / twos.value
+                state = twoNumberText.innerHTML
+                final.innerHTML = 1 + " " + one.value + " = " + ones.value / twos.value + " " + two.value
+            } else if (ones.value < twos.value) {
+                twoNumberText.innerHTML = twos.value / ones.value
+                state = twoNumberText.innerHTML
+                final.innerHTML = 1 + " " + one.value + " = " + twos.value / ones.value + " " + two.value
+            }
+
+        }); 
 
 })
 
-oneNumberText.addEventListener("input",function(){
-    console.log(oneNumberText.value);
-    
-   
 
-    fetch("money.json")
-    .then(response => response.json())
-    .then(data => {
-        twoNumberText.innerHTML = data.find(item => item.name === two.value).value;
 
-         twoNumberText.innerHTML = twoNumberText.innerHTML * oneNumberText.value
-    });
-    
+oneNumberText.addEventListener("input", function () {
+
+    twoNumberText.innerHTML = state * oneNumberText.value
+
 })
