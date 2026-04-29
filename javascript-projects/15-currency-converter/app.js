e302f55e3eca8aaa09faf43765f800a8f0f74a85
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2026-04-29 11:41:45 +0300

    updated currency-converter
    JS

--- javascript-projects/15-currency-converter/app.js
+++ javascript-projects/15-currency-converter/app.js
@@ -10,54 +10,32 @@ let state = 0;
 
 one.addEventListener("click", function (e) {
 
-      fetch("money.json")
+    fetch("money.json")
         .then(response => response.json())
         .then(data => {
             let ones = data.find(item => item.name === one.value);
             let twos = data.find(item => item.name === two.value);
 
-            if (ones.value === twos.value) {
-                twoNumberText.innerHTML = 1;
-                state = twoNumberText.innerHTML;
-                final.innerHTML = 1 + " " + one.value + " = " + 1 + " " + two.value
-            } else if (ones.value > twos.value) {
-                twoNumberText.innerHTML = (twos.value / ones.value).toFixed(4)
-                state = twoNumberText.innerHTML
-                final.innerHTML = 1 + " " + one.value + " = " + (twos.value / ones.value).toFixed(4) + " " + two.value
-            } else if (ones.value < twos.value) {
-                twoNumberText.innerHTML = (twos.value / ones.value).toFixed(4)
-                state = twoNumberText.innerHTML
-                final.innerHTML = 1 + " " + one.value + " = " + (twos.value / ones.value).toFixed(4) + " " + two.value
-            }
+            twoNumberText.innerHTML = (ones.value / twos.value).toFixed(4)
+            state = twoNumberText.innerHTML
+            final.innerHTML = 1 + " " + one.value + " = " + (ones.value / twos.value).toFixed(4) + " " + two.value
 
-            console.log(ones.value + " - " + twos.value);
-        });     
+        });
 })
 
 
 two.addEventListener("click", function (e) {
-  fetch("money.json")
+     fetch("money.json")
         .then(response => response.json())
         .then(data => {
             let ones = data.find(item => item.name === one.value);
             let twos = data.find(item => item.name === two.value);
 
-            if (ones.value === twos.value) {
-                twoNumberText.innerHTML = 1;
-                state = twoNumberText.innerHTML;
-                final.innerHTML = 1 + " " + one.value + " = " + 1 + " " + two.value
-            } else if (ones.value > twos.value) {
-                twoNumberText.innerHTML = (twos.value / ones.value).toFixed(4)
-                state = twoNumberText.innerHTML
-                final.innerHTML = 1 + " " + one.value + " = " + (twos.value / ones.value).toFixed(4) + " " + two.value
-            } else if (ones.value < twos.value) {
-                twoNumberText.innerHTML = (twos.value / ones.value).toFixed(4)
-                state = twoNumberText.innerHTML
-                final.innerHTML = 1 + " " + one.value + " = " + (twos.value / ones.value).toFixed(4) + " " + two.value
-            }
+            twoNumberText.innerHTML = (ones.value / twos.value).toFixed(4)
+            state = twoNumberText.innerHTML
+            final.innerHTML = 1 + " " + one.value + " = " + (ones.value / twos.value).toFixed(4) + " " + two.value
 
-            console.log(ones.value + " - " + twos.value);
-        });    
+        });
 
 })
 
